875c65df6d773f45d96c62640b3601d2cd66aee0
Zakydawamul <zakydb091005@gmail.com>
2024-10-01 14:32:37 +0700

    menambahkan hasil

--- Pemilihan2Percobaan107.java
+++ Pemilihan2Percobaan107.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
+
 public class Pemilihan2Percobaan107 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Masukkan tahun: ");
+        System.out.print("Masukkan tahun: ");
         int tahun = input.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat"); 
-                if ((tahun % 400) == 0) {
-                    System.out.println("Tahun kabisat");
-                } else 
+        // Tahun kabisat
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
                     System.out.println("Bukan Tahun Kabisat");
-        }  else 
+                }
+            } else {
+                
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+        }
+
         input.close();
     }
-
-    
-        
 }
