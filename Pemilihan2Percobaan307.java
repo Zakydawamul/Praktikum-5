f21be3ef71c6285883444bd4808624b38034f402
Zakydawamul <zakydb091005@gmail.com>
2024-10-22 13:47:05 +0700

    jobsheet 8

---
+++ Pemilihan2Percobaan307.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan307 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+
+        System.out.println("Masukkan Kategori (pekerja/pebisnis): ");
+        kategori = input.nextLine();
+        System.out.println("Masukkan Besarnya Penghasilan: ");
+        penghasilan = input.nextInt();
+
+        input.close();
+        System.out.println("========== PENGHITUNGAN PAJAK PENGHASILAN ===========");
+
+        if (kategori.equals("pekerja")) {
+            if (penghasilan <= 2000000) 
+                pajak = 0.1;
+            else if (penghasilan <= 3000000) 
+                pajak = 0.15;
+            else     
+                pajak = 0.2;
+
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih: " + gajiBersih);     
+        } else if (kategori.equals("pebisnis")) {
+            if (penghasilan <= 2500000) 
+                pajak = 0.15;
+            else if (penghasilan <= 3500000)
+                pajak = 0.2;
+            else 
+                pajak = 0.25;
+
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.println("Penghasilan Bersih: " + gajiBersih);            
+        } else {
+            System.out.println("Masukkan kategori salah");
+        }
+    }
+}
